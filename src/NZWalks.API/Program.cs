61d588385591190fc6897208acadfdcfefb2c376
habib24457 <habiburehman390@gmail.com>
2024-11-09 13:37:47 +0100

    clean up program.cs

--- src/NZWalks.API/Program.cs
+++ src/NZWalks.API/Program.cs
@@ -4,11 +4,7 @@ using NZWalks.API.Mappings;
 using NZWalks.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -22,7 +18,6 @@ builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
