21ebe5ca035fcbac74a29a42ff1556d316c10886
habib24457 <habiburehman390@gmail.com>
2025-05-24 12:22:45 +0200

    wip

--- src/NZWalks.API/Program.cs
+++ src/NZWalks.API/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("https://newwalkzone.netlify.app")
+            .WithOrigins("https://newwalkzone.netlify.app/")
             .AllowAnyMethod()
             .AllowAnyHeader();
     });
